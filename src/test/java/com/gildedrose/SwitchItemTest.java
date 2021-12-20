c510ac48749875810983ddaf1ece6f4b6b155ebc
menno-glitch <mennosaey@hotmail.com>
2021-12-20 23:18:17 +0100

    switch item now cannot go under value 0

--- src/test/java/com/gildedrose/SwitchItemTest.java
+++ src/test/java/com/gildedrose/SwitchItemTest.java
@@ -2,53 +2,67 @@ package com.gildedrose;
 
 import com.gildedrose.core.GildedRose;
 import com.gildedrose.core.Item;
-import com.gildedrose.items.NormalItem;
 import com.gildedrose.items.SwitchItem;
 import org.junit.jupiter.api.Test;
 
 public class SwitchItemTest {
 
-    private Item[] items = new Item[] {new SwitchItem("switch item", 10, 20)};
+    private Item[] items = new Item[]{new SwitchItem("switch item", 10, 20)};
     private GildedRose app = new GildedRose(items);
 
     @Test
-    void decreaseQualityNormal() {
-        app.calculateNextDay();
-        assert(app.items[0].sellIn == 9);
-        assert(app.items[0].quality == 21);
-        app.calculateNextDay();
-        assert(app.items[0].sellIn == 8);
-        assert(app.items[0].quality == 22);
-    }
+        void increaseQualityNormal() {
+            app.calculateNextDay();
+            assert (app.items[0].sellIn == 9);
+            assert (app.items[0].quality == 21);
+            app.calculateNextDay();
+            assert (app.items[0].sellIn == 8);
+            assert (app.items[0].quality == 22);
+        }
 
-    @Test
-    void noQualityAbove50() {
-        app.items[0].sellIn = 10;
-        app.items[0].quality = 49;
-        app.calculateNextDay();
-        assert(app.items[0].sellIn == 9);
-        assert(app.items[0].quality == 50);
-        app.calculateNextDay();
-        assert(app.items[0].sellIn == 8);
-        assert(app.items[0].quality == 50);
-        app.calculateNextDay();
-        assert(app.items[0].sellIn == 7);
-        assert(app.items[0].quality == 50);
-    }
+        @Test
+        void noQualityAbove50() {
+            app.items[0].sellIn = 10;
+            app.items[0].quality = 49;
+            app.calculateNextDay();
+            assert (app.items[0].sellIn == 9);
+            assert (app.items[0].quality == 50);
+            app.calculateNextDay();
+            assert (app.items[0].sellIn == 8);
+            assert (app.items[0].quality == 50);
+            app.calculateNextDay();
+            assert (app.items[0].sellIn == 7);
+            assert (app.items[0].quality == 50);
+        }
 
-    @Test
-    void increaseQualityExpired() {
-        app.items[0].sellIn = 1;
-        app.items[0].quality = 20;
-        app.calculateNextDay();
-        assert(app.items[0].sellIn == 0);
-        assert(app.items[0].quality == 21);
-        app.calculateNextDay();
-        assert(app.items[0].sellIn == -1);
-        assert(app.items[0].quality == 20);
-        app.calculateNextDay();
-        assert(app.items[0].sellIn == -2);
-        assert(app.items[0].quality == 19);
-    }
+        @Test
+        void increaseQualityExpired() {
+            app.items[0].sellIn = 1;
+            app.items[0].quality = 20;
+            app.calculateNextDay();
+            assert (app.items[0].sellIn == 0);
+            assert (app.items[0].quality == 21);
+            app.calculateNextDay();
+            assert (app.items[0].sellIn == -1);
+            assert (app.items[0].quality == 20);
+            app.calculateNextDay();
+            assert (app.items[0].sellIn == -2);
+            assert (app.items[0].quality == 19);
+        }
 
-}
+        @Test
+        void noQualityBelowZero() {
+            app.items[0].sellIn = 1;
+            app.items[0].quality = 0;
+            app.calculateNextDay();
+            assert(app.items[0].sellIn == 0);
+            assert(app.items[0].quality == 1);
+            app.calculateNextDay();
+            assert(app.items[0].sellIn == -1);
+            assert(app.items[0].quality == 0);
+            app.calculateNextDay();
+            assert(app.items[0].sellIn == -2);
+            assert(app.items[0].quality == 0);
+        }
+
+    }
